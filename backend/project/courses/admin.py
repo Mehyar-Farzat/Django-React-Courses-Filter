fcbb34cf372cf92d97cf03773a0d7e111aadc766
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-28 12:47:39 +0100

    register models in admin.py

--- backend/project/courses/admin.py
+++ backend/project/courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Courses
 
 # Register your models here.
+
+admin.site.register(Coueses)
+admin.site.register(Category)
--- backend/project/courses/models.py
+++ backend/project/courses/models.py
@@ -19,7 +19,7 @@ class Courses(models.Model):
     description = models.TextField(max_length=3000)
     image = models.ImageField(upload_to='courses')
     price = models.IntegerField()
-    category = models.models.ForeignKey("Category", related_name='course_category',on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey("Category", related_name='course_category',on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
